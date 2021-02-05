4cb31cf454506fa9b74b84069cb02468c5303202
Apenasgabs <gabers357@gmail.com>
2021-02-04 21:26:32 -0300

    chore: started the dom management

--- scripts.js
+++ scripts.js
@@ -9,11 +9,28 @@ const Modal = {
 
     }
 } 
-const transaction = [{
-    id:1,
-    description: 'Luz',
-    amount: ,
-}]
+const transactions = [
+    {
+        id:1,
+        description: 'Luz',
+        amount: -25000 ,
+        date:'23/01/2021'
+    },
+    {
+        id:2,
+        description: 'Freela',
+        amount: 50000 ,
+        date:'23/01/2021'
+    },
+    {
+        id:3,
+        description: 'Internet',
+        amount: -10000 ,
+        date:'23/01/2021'
+    },
+
+
+]
 
 const Transaction = {
     incomes(){
@@ -25,3 +42,27 @@ const Transaction = {
     total(){
         //ve o que sobra entre os dois 
     }
+}
+
+const DOM = {
+    addTransaction(transaction,index){
+        console.log(transaction)
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction()
+    },
+    innerHTMLTransaction(){
+        const html = `
+        
+            <td class="description">Luz</td>
+            <td class="expense">R$500</td>
+            <td class="date">23/05/2022</td>
+            <td>
+                <img src="/assets/minus.svg" alt="Remover Transações">
+            </td>
+        
+        `
+        return html
+    }
+}
+
+DOM.addTransaction(transactions[2])
